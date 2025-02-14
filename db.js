1235f3bc1737a81ff1dac37fcbbc82bcb80a1bf8
choimiae <chlaldo1220@gmail.com>
2025-02-14 17:22:06 +0900

    db 연결풀로 변경

--- db.js
+++ db.js
@@ -1,20 +1,27 @@
 const mysql = require('mysql2/promise');
 
+const pool = mysql.createPool({
+	host:"localhost",
+	port: 3306,
+	user: 'root',
+	password: 'root',
+	database: 'nomory_db',
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
+});
+
 const connectDB = async () => {
 	try {
-		const connect = await mysql.createConnection({
-			host:"localhost",
-			port: 3306,
-			user: 'root',
-			password: 'root',
-			database: 'nomory_db'
-		});
+		const connect = await pool.getConnection();
 
-		console.log("✅ DB CONNECTED");
+		console.log("✅ Success - DB connected");
+
+		connect.release();
 
 		return connect;
 	} catch (err) {
-		console.error("❌ FAIL - DB CONNECTION :: \n", err);
+		console.error("❌ Fail - DB connected :: \n", err);
 		throw err;
 	}
 }
