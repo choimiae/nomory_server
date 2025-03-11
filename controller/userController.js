39fdb43c022e6668c68e244af52a7f1e5acccb65
choimiae <chlaldo1220@gmail.com>
2025-03-11 16:39:21 +0900

    비밀번호 해싱 추가

--- controller/userController.js
+++ controller/userController.js
@@ -1,16 +1,21 @@
+const bcrypt = require('bcrypt');
 const connectDB = require('../config/db');
 const utils = require('../utils/utils');
 
+const hashPw = async (password) => {
+	return await bcrypt.hash(password, 10)
+}
 
 // 회원가입
 const addUser = async (req, res, next) => {
 	const { id, password, nickname } = req.body;
+	const hashPassword = await hashPw(password);
 	const sql = 'INSERT INTO user_list(id, password, nickname, date) VALUES (?, ?, ?, ?)';
 
 	try {
 		const connect = await connectDB();
 		const nowTime = utils.getCurrentTime();
-		await connect.execute(sql, [id, password, nickname, nowTime]);
+		await connect.execute(sql, [id, hashPassword, nickname, nowTime]);
 		res.status(201).json({ message: 'Success - data insert'});
 	} catch (err) {
 		next(utils.throwError('Fail - data insert', 500));
