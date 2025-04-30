981cb2e22a7dddd8a4f50371836393bb6af267b1
choimiae <chlaldo1220@gmail.com>
2025-04-30 15:18:59 +0900

    장소 관련 사용자 권한 체크

--- controller/placeController.js
+++ controller/placeController.js
@@ -5,11 +5,15 @@ const utils = require('../utils/utils');
 // 장소 검색
 const getPlace = async (req, res, next) => {
 	const { idx } = req.query;
-	let sql = idx ? 'SELECT * FROM place_list WHERE IDX = ?' : 'SELECT * FROM place_list';
+	const { id } = req.user;
+	let sql = 'SELECT * FROM place_list WHERE ID = ?';
+
+	if(idx)
+		sql = 'SELECT * FROM place_list WHERE ID = ? AND IDX = ?';
 
 	try {
 		const connect = await connectDB();
-		const [rows] = await connect.execute(sql, idx ? [idx] : []);
+		const [rows] = await connect.execute(sql, idx ? [id, idx] : [id]);
 		res.status(200).json({ message: '장소 검색이 완료되었습니다.', data: rows });
 	} catch (err) {
 		next(utils.throwError('장소 검색 오류가 발생했습니다..', 500));
@@ -19,12 +23,13 @@ const getPlace = async (req, res, next) => {
 // 장소 등록
 const addPlace = async (req, res, next) => {
 	const { idx, pos_lat, pos_lng, title, addr, date, memo, rating } = req.body;
-	const sql = 'INSERT INTO place_list(idx, name, title, pos_lat, pos_lng, addr, memo, date, rating, reg_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+	const { id } = req.user;
+	const sql = 'INSERT INTO place_list(idx, id, title, pos_lat, pos_lng, addr, memo, date, rating, reg_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
 
 	try {
 		const connect = await connectDB();
 		const nowTime = utils.getCurrentTime();
-		await connect.execute(sql, [idx, 'name', title, pos_lat, pos_lng, addr, memo, date, rating, nowTime]);
+		await connect.execute(sql, [idx, id, title, pos_lat, pos_lng, addr, memo, date, rating, nowTime]);
 		res.status(201).json({ message: '장소 등록이 완료되었습니다.'});
 	} catch (err) {
 		next(utils.throwError('장소 등록 오류가 발생했습니다.', 500));
--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const authToken = require('../middleware/authMiddleware');
 const placeController = require('../controller/placeController');
 
-router.get('/', placeController.getPlace);
-router.post('/', placeController.addPlace);
-router.patch('/', placeController.updatePlace);
-router.delete('/', placeController.deletePlace);
+router.get('/', authToken, placeController.getPlace);
+router.post('/', authToken, placeController.addPlace);
+router.patch('/', authToken, placeController.updatePlace);
+router.delete('/', authToken, placeController.deletePlace);
 
 module.exports = router;
