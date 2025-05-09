8bb78d3d0c0e6d996a531e8f1f15b7b3df16b193
choimiae <chlaldo1220@gmail.com>
2025-05-09 10:40:11 +0900

    aws rds 변경

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-	host:"localhost",
+	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
